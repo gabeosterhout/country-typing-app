728c87e459b053b5939657b7f274d1ec8a1dfc26
Gabe Osterhout <78832250+gabeosterhout@users.noreply.github.com>
2025-03-23 16:28:33 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TypingTest from './TypingTest';
+
+function App() {
+  return (
+    <div className="App">
+      <TypingTest />
+    </div>
+  );
+}
+
+export default App;
